9cdfae12320fb2973455e298080b0367ca7dab53
Anna Pardo <haberan2@msu.edu>
2023-05-11 12:45:19 -0400

    comments on python script

--- scripts/mapping_rate_exploration/find_map_rates.py
+++ scripts/mapping_rate_exploration/find_map_rates.py
@@ -2,37 +2,44 @@
 
 __author__ = "Anna Pardo"
 
+# import modules
 import argparse
 import os
 import pandas as pd
 import json
 import statistics
 
+# write a function to generate a dataframe with filepaths and mapping rates from Salmon
 def make_df(salmon_dir):
+        # salmon_dir is the full path to the directory containing Salmon outputs
+
+        # initialize empty lists
 	percent_mapped = []
 	sample_info = []
 	salmon_file = []
+
+        # iterate through subdirectories of salmon_dir
 	for folder in os.listdir(salmon_dir):
+                # this is added for data downloaded from SRA
 		if folder.startswith("SRR"):
+                        # make a variable containing the path to the meta_info.json file
 			json_file = salmon_dir+folder+"/aux_info"+"/meta_info.json"
-			#print(json_file)
+			# open meta_info.json, pull out path to quant.sf file, sample name, and mapping rate, and append to previously initialized lists
 			with open(json_file, "r") as infile:
 				meta_info = json.load(infile)
 				map_percent = meta_info["percent_mapped"]
 				dir_split = json_file.strip().split("/")
-				#print(dir_split)
 				name_info = dir_split[8]
-				print(name_info)
-				#if name_info.endswith("_1"):
-				#	name_info = name_info.split("_")[0]
 				sample_info.append(name_info)
 				percent_mapped.append(map_percent)
 				salmon_file.append(salmon_dir+folder+"/quant.sf")
+        # make dataframe and return dataframe
 	df = pd.DataFrame({"File":salmon_file,"Sample":sample_info,"Percent_Mapped":percent_mapped})
 	return df
 
 def main():
 	parser = argparse.ArgumentParser(description="Parse args")
+        # command line arguments
 	parser.add_argument("--directory","-d",type=str,help="full path to directory containing salmon output files")
 	parser.add_argument("--outfilename","-o",type=str,help="full path to desired output file for mapping rate json")
 	args = parser.parse_args()
@@ -40,9 +47,8 @@ def main():
 	outfilename = str(args.outfilename)
 
 	df = make_df(directory)
-	print(df.head())
 
-	# print the statistics
+	# print the summary statistics of mapping rates
 	print("maximum mapping rate:",max(df["Percent_Mapped"]))
 	print("minimum mapping rate:",min(df["Percent_Mapped"]))
 	print("mean mapping rate:",sum(df["Percent_Mapped"])/len(df.index))
